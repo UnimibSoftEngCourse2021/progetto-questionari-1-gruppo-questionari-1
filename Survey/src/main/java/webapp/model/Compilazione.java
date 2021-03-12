b8dee7bd440fda5f76ed9457e7b360a170fc19cc
simone20b <s.valentini11@campus.unimib.it>
2021-03-12 12:45:01 +0100

    pulizia import non utilizzati

--- Survey/src/main/java/webapp/model/Compilazione.java
+++ Survey/src/main/java/webapp/model/Compilazione.java
@@ -3,12 +3,10 @@ package webapp.model;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
